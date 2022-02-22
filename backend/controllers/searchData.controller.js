4dd64eaa5840ad92c4cab0f239ec9ac993b8845b
Konrad Jówko <konrad.jowko@gmail.com>
2022-02-22 23:01:45 +0100

    Add controller getting searched URL data from Ipstack

--- backend/controllers/searchData.controller.js
+++ backend/controllers/searchData.controller.js
@@ -1,10 +1,30 @@
-exports.get = async (req, res) => {
-  try{
-    console.log(req.params.adress);
+const Axios = require('axios');
 
-    res.json(req.params.adress);
-  }
-  catch(err) {
-    res.status(500).json(err);
-  }
+exports.get = async (req, res) => {
+  Axios
+    .get(`http://api.ipstack.com/${req.params.adress}?access_key=${process.env.ipstack}`)
+    .then(response => {
+      const geoData = {
+        adressData: [],
+        geolocation: {},
+      };
+
+      geoData.adressData.push({name: response.data.type , content: response.data.ip});
+      geoData.adressData.push({name: 'Continent' , content: response.data.continent_code});
+      geoData.adressData.push({name: 'Country' , content: response.data.country_name});
+      geoData.adressData.push({name: 'Region' , content: response.data.region_name});
+      geoData.adressData.push({name: 'City' , content: response.data.city});
+      if (response.data.zip) {geoData.adressData.push({name: 'Zip' , content: response.data.zip});}
+
+      geoData.geolocation.latitude = response.data.latitude;
+      geoData.geolocation.longitude = response.data.longitude;
+
+      res.json(geoData);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 };
+
+
+// `http://api.ipstack.com/${req.params.adress}?access_key=${IPSTACK}`
--- backend/controllers/userIp.controller.js
+++ backend/controllers/userIp.controller.js
@@ -1,6 +1,9 @@
 exports.get = async (req, res) => {
   try{
-    res.json(req.headers['x-forwarded-for']);
+    console.log(req.ip);
+
+    res.json(req.ip);
+    // res.json(req.headers['x-forwarded-for']);
   }
   catch(err) {
     res.status(500).json(err);
