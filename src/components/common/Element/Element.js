4119adb0e276b77e43e06918edebe39fbf295eac
Konrad Jówko <konrad.jowko@gmail.com>
2022-02-23 19:43:42 +0100

    Fix bug with re-searching query from history

--- src/components/common/Element/Element.js
+++ src/components/common/Element/Element.js
@@ -6,14 +6,14 @@ import styles from './Element.module.scss';
 /* COMPONENT PRESENTING ALREADY SEARCH ADRESS*/
 const Element = ({content, getSearch}) => {
   const manageClick = (event) => {
-    const adress = event.target.childNodes[0].innerHTML;
+    const adress = event.target.childNodes[0].textContent;
 
     getSearch(adress, true);
   };
 
   return (
     <div className={styles.container} onClick={manageClick}>
-      <h1 className={styles.title}> {content}</h1>
+      <h1 className={styles.title}>{content}</h1>
       <h1 className={styles.hoverTitle}> Search again </h1>
     </div>
   );
